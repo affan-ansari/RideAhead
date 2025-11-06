8a4026436360684190ab4d3979f00992d5c74ebc
Affan Arif <affanarif74@gmail.com>
2025-11-06 11:48:31 +0500

    Setup basic navigation:
    * installed and configured navigation packages
    * created directory structure for auth navigation
    * created placeholder login and signup screens

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
-import { StatusBar, Text, useColorScheme } from 'react-native';
+import { StatusBar, useColorScheme } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import AppNavigator from './src/navigation/AppNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Text>Ride Ahead App</Text>
+      <GestureHandlerRootView>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </GestureHandlerRootView>
     </SafeAreaProvider>
   );
 }
---
+++ src/navigation/AuthNavigator.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import type { AuthStackParamList } from './types';
+import { LoginScreen } from '../screens/auth/LoginScreen';
+import { SignupScreen } from '../screens/auth/SignupScreen';
+
+const Stack = createNativeStackNavigator<AuthStackParamList>();
+
+export default function AuthNavigator() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Login"
+      screenOptions={{
+        headerShadowVisible: false,
+      }}
+    >
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ title: 'Sign In' }}
+      />
+      <Stack.Screen
+        name="Signup"
+        component={SignupScreen}
+        options={{ title: 'Sign Up' }}
+      />
+    </Stack.Navigator>
+  );
+}
